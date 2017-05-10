653c83d9dc26f766258c825e7a0f18cc3fd1695e
Hagai Kariti <hkariti@gmail.com>
2017-05-10 20:40:26 +0300

    bug fix

--- wet2/UART_Rx.m
+++ wet2/UART_Rx.m
@@ -1,11 +1,12 @@
 function [ hex_info ] = UART_Rx( samples )
+hex_info = [];
 current_ind = 1;
-hex_info = "";
-while ( current_ind < length(samples) )
-    posedge = find_first_posedge(samples, current_ind);
+posedge = find_first_posedge(samples, current_ind);
+while ( ~isempty(posedge) )
     bin_info = extract_info(samples(posedge:posedge+59));
     hex_info = strcat(hex_info, bin2hex(bin_info));
-    current_ind = posedge + 60
+    current_ind = posedge + 60;
+    posedge = find_first_posedge(samples, current_ind);
 end
 end
 
