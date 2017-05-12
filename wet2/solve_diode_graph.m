4df57a91e8c30c67baa4be47cb48d46bfc856124
Hagai Kariti <hkariti@gmail.com>
2017-05-12 18:51:36 +0300

    question 2

--- wet2/solve_diode_graph.m
+++ wet2/solve_diode_graph.m
@@ -1,6 +1,18 @@
 function [ Vd , Id ] = solve_diode_graph( Ieq , Req , Vd_vec )
+I_0 = 1e-12;
+KTq = 0.026;
+f1 = @(Vd)I_0*(exp(Vd/KTq) - 1);
+f2 = @(Vd)Ieq - Vd/Req;
 
+I1 = f1(Vd_vec);
+I2 = f2(Vd_vec);
 
+diff = abs(I1-I2);
+[~, min_pos] = min(diff);
+Vd = Vd_vec(min_pos);
+Id = I1(min_pos);
 
+plot(Vd_vec, I1, 'b', Vd_vec, I2, 'g');
+xlim([Vd_vec(1), Vd_vec(round(1.1*min_pos))]);
 end
 
--- wet2/solve_diode_iter.m
+++ wet2/solve_diode_iter.m
@@ -1,6 +1,26 @@
 function [ Vd , Id ] = solve_diode_iter( Ieq , Req  )
-
-
-
+% Constants
+MAX_RUNS = 7;
+V_ACCURACY = 1e-4;
+KTq = 0.026;
+I_0 = 1e-12;
+f_rev = @(I)KTq*log(I/I_0 + 1);
+V_0 = Ieq*Req/2;
+% Init vars
+Vd = [];
+Id = [];
+% Start running
+currentV = V_0;
+run = 1;
+still_running = true;
+while (still_running)
+    prevV = currentV;
+    currentI = Ieq - prevV/Req;
+    currentV = f_rev(currentI);
+    run = run + 1;
+    still_running = (abs(currentV - prevV) >= V_ACCURACY ) && ...
+        run <= MAX_RUNS;
 end
-
+Vd = currentV;
+Id = currentI;
+end
