ebd8063000275f5cc7abbe167c9d056ae541fbfc
Hagai Kariti <hkariti@gmail.com>
2017-05-13 19:08:45 +0300

    finish diode_graph;; fixes in diode_iter

--- wet2/solve_diode_graph.m
+++ wet2/solve_diode_graph.m
@@ -1,4 +1,5 @@
 function [ Vd , Id ] = solve_diode_graph( Ieq , Req , Vd_vec )
+% Calculate Id in both ways
 I_0 = 1e-12;
 KTq = 0.026;
 f1 = @(Vd)I_0*(exp(Vd/KTq) - 1);
@@ -7,12 +8,25 @@ f2 = @(Vd)Ieq - Vd/Req;
 I1 = f1(Vd_vec);
 I2 = f2(Vd_vec);
 
+% Find intersection
 diff = abs(I1-I2);
 [~, min_pos] = min(diff);
 Vd = Vd_vec(min_pos);
 Id = I1(min_pos);
-
-plot(Vd_vec, I1, 'b', Vd_vec, I2, 'g');
-xlim([Vd_vec(1), Vd_vec(round(1.1*min_pos))]);
+% Calculate graph limits
+lower_limit = min_pos - 10;
+higher_limit = min_pos + 10;
+if (lower_limit < 1)
+    lower_limit = 1;
+end
+if (higher_limit > length(Vd_vec))
+    higher_limit = length(Vd_vec);
+end
+y_limits = sort([I2(higher_limit), I2(lower_limit)]);
+x_limits = [Vd_vec(lower_limit), Vd_vec(higher_limit)];
+% Plot graph
+plot(Vd_vec, [I1; I2], '-x');
+xlim([x_limits(1), x_limits(2)]);
+ylim([y_limits(1), y_limits(2)]);
 end
 
--- wet2/solve_diode_iter.m
+++ wet2/solve_diode_iter.m
@@ -6,9 +6,6 @@ KTq = 0.026;
 I_0 = 1e-12;
 f_rev = @(I)KTq*log(I/I_0 + 1);
 V_0 = Ieq*Req/2;
-% Init vars
-Vd = [];
-Id = [];
 % Start running
 currentV = V_0;
 run = 1;
@@ -18,9 +15,15 @@ while (still_running)
     currentI = Ieq - prevV/Req;
     currentV = f_rev(currentI);
     run = run + 1;
-    still_running = (abs(currentV - prevV) >= V_ACCURACY ) && ...
-        run <= MAX_RUNS;
+    Vdiff = abs(currentV - prevV);
+    still_running = (Vdiff >= V_ACCURACY ) && run <= MAX_RUNS;
 end
-Vd = currentV;
-Id = currentI;
+% Check if we converged after the previous runs
+if (Vdiff >= V_ACCURACY)
+    Vd = [];
+    Id = [];
+else
+    Vd = currentV;
+    Id = currentI;
+end
 end
