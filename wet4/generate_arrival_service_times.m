ebdb4f1dc45b3a7fa93c101faf3105cc29483e12
Hagai Kariti <hkariti@gmail.com>
2017-07-01 22:32:26 +0300

    column vecetor

--- wet4/generate_arrival_service_times.m
+++ wet4/generate_arrival_service_times.m
@@ -3,8 +3,8 @@ function [ arrival_times , service_times ] = generate_arrival_service_times( lam
 vector_entry = @(p) -1./p * log(1-rand());
 num_requests = 10;
 time_diffs = vector_entry(linspace(lambda, lambda, num_requests));
-arrival_times = cumsum(time_diffs);
-service_times = vector_entry(linspace(mu, mu, num_requests));
+arrival_times = transpose(cumsum(time_diffs));
+service_times = transpose(vector_entry(linspace(mu, mu, num_requests)));
 
 end
 
